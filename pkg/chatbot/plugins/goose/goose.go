b128d57f56134da26dbc47c9ee6cbedba253d3b7
charles-edouard.breteche <charles.breteche@agriconomie.com>
2020-09-21 15:59:25 +0200

    goose and wip plugins

--- pkg/chatbot/plugins/goose/goose.go
+++ pkg/chatbot/plugins/goose/goose.go
@@ -1,124 +1,101 @@
-/*
-Copyright 2019 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Package goose adds goose images to an issue or PR in response to a /honk comment
 package goose
 
-// type scmClient interface {
-// 	CreateComment(string, int, string) error
-// }
+import (
+	"errors"
+	"fmt"
+	"net/url"
 
-// type scmTools interface {
-// 	ImageTooBig(string) (bool, error)
-// 	QuoteAuthorForComment(string) string
-// }
+	"github.com/eddycharly/kloops/api/v1alpha1"
+	"github.com/eddycharly/kloops/pkg/chatbot/plugins"
+	"github.com/eddycharly/kloops/pkg/clients/unsplash"
+	"github.com/eddycharly/kloops/pkg/scmprovider"
+	"github.com/eddycharly/kloops/pkg/utils"
+	"github.com/jenkins-x/go-scm/scm"
+)
 
-// const (
-// 	pluginName = "goose"
-// )
+const (
+	pluginName = "goose"
+)
 
-// var (
-// 	match = regexp.MustCompile(`(?mi)^/(honk)\s*$`)
-// )
+var (
+	plugin = plugins.Plugin{
+		Description:        "The goose plugin adds a goose image to an issue or PR in response to the `/honk` command.",
+		ConfigHelpProvider: configHelp,
+		Commands: []plugins.Command{{
+			Name:        "honk",
+			Description: "Add a goose image to the issue or PR",
+			Action: plugins.
+				Invoke(handle).
+				When(plugins.Action(scm.ActionCreate)),
+		}},
+	}
+)
 
-// func init() {
-// 	plugins.RegisterHelpProvider(pluginName, helpProvider)
-// 	plugins.RegisterIssueCommentHandler(pluginName, handleIssueComment)
-// 	plugins.RegisterPullRequestCommentHandler(pluginName, handlePullRequestComment)
-// }
+func init() {
+	plugins.RegisterPlugin(pluginName, plugin)
+}
 
-// func helpProvider(config *v1alpha1.PluginConfigSpec) (*pluginhelp.PluginHelp, error) {
-// 	pluginHelp := &pluginhelp.PluginHelp{
-// 		Description: "The goose plugin adds a goose image to an issue or PR in response to the `/honk` command.",
-// 		Config: map[string]string{
-// 			"": "The goose plugin uses an api key for unsplash.com stored in the plugin config",
-// 		},
-// 	}
-// 	pluginHelp.AddCommand(pluginhelp.Command{
-// 		Usage:       "/honk",
-// 		Description: "Add a goose image to the issue or PR",
-// 		Featured:    false,
-// 		WhoCanUse:   "Anyone",
-// 		Examples:    []string{"/honk"},
-// 	})
-// 	return pluginHelp, nil
-// }
+func configHelp(config *v1alpha1.PluginConfigSpec) (map[string]string, error) {
+	return map[string]string{
+			"": "The goose plugin uses an api key for unsplash.com stored in the plugin config",
+		},
+		nil
+}
 
-// func handleIssueComment(request plugins.PluginRequest, event *scm.IssueCommentHook) error {
-// 	scmClient := request.ScmClient()
-// 	return handle(scmClient.Issues, scmClient.Tools, request.Logger(), event.Repo, event.Action, event.Comment, event.Issue.Number, getKey(request))
-// }
+func handle(match plugins.CommandMatch, request plugins.PluginRequest, event plugins.GenericCommentEvent) error {
+	logger := request.Logger()
+	scmClient := request.ScmClient()
+	// Fetch image
+	image, err := fetchImage(scmClient.Tools, getKey(request)())
+	if err != nil {
+		logger.Error(err, "Failed to get cat img")
+		return err
+	}
+	// Format the response comment
+	rspn, err := formatResponse(image)
+	if err != nil {
+		logger.Error(err, "Failed to format response")
+		return err
+	}
+	// Create comment
+	if event.IsPR {
+		return scmClient.PullRequests.CreateComment(event.Repo.FullName, event.Number, plugins.FormatResponseRaw(scmClient.Tools, event.Body, event.Link, event.Author.Login, rspn))
+	}
+	return scmClient.Issues.CreateComment(event.Repo.FullName, event.Number, plugins.FormatResponseRaw(scmClient.Tools, event.Body, event.Link, event.Author.Login, rspn))
+}
 
-// func handlePullRequestComment(request plugins.PluginRequest, event *scm.PullRequestCommentHook) error {
-// 	scmClient := request.ScmClient()
-// 	return handle(scmClient.PullRequests, scmClient.Tools, request.Logger(), event.Repo, event.Action, event.Comment, event.PullRequest.Number, getKey(request))
-// }
+func getKey(request plugins.PluginRequest) func() string {
+	return func() string {
+		pluginConfig := request.PluginConfig()
+		if pluginConfig == nil {
+			return ""
+		}
+		key, err := utils.GetSecret(request.Client(), request.RepoConfig().Namespace, pluginConfig.Goose.Key)
+		if err == nil {
+			return string(key)
+		}
+		fmt.Println(err)
+		return ""
+	}
+}
 
-// func handle(client scmClient, scmTools scmTools, logger logr.Logger, repo scm.Repository, action scm.Action, comment scm.Comment, number int, getKey func() string) error {
-// 	// Only consider new comments.
-// 	if action != scm.ActionCreate {
-// 		return nil
-// 	}
-// 	// Make sure they are requesting a goose
-// 	mat := match.FindStringSubmatch(comment.Body)
-// 	if mat == nil {
-// 		return nil
-// 	}
-// 	// Fetch image
-// 	image, err := fetchImage(scmTools, getKey())
-// 	if err != nil {
-// 		logger.Error(err, "Failed to get cat img")
-// 		return err
-// 	}
-// 	// Format the response comment
-// 	rspn, err := formatResponse(image)
-// 	if err != nil {
-// 		logger.Error(err, "Failed to format response")
-// 		return err
-// 	}
-// 	return client.CreateComment(repo.FullName, number, plugins.FormatCommentResponse(scmTools, comment, rspn))
-// }
+func fetchImage(scmTools scmprovider.Tools, key string) (string, error) {
+	for i := 0; i < 3; i++ {
+		image, err := unsplash.Search("goose", key)
+		if err == nil {
+			toobig, err := scmTools.ImageTooBig(image)
+			if err == nil && !toobig {
+				return image, nil
+			}
+		}
+	}
+	return "", errors.New("Failed to find a goose image")
+}
 
-// func getKey(request plugins.PluginRequest) func() string {
-// 	return func() string {
-// 		key, err := utils.GetSecret(request.Client(), request.RepoConfig().Namespace, request.PluginConfig().Goose.Key)
-// 		if err == nil {
-// 			return string(key)
-// 		}
-// 		return ""
-// 	}
-// }
-
-// func fetchImage(scmTools scmTools, key string) (string, error) {
-// 	for i := 0; i < 3; i++ {
-// 		image, err := unsplash.Search("goose", key)
-// 		if err == nil {
-// 			toobig, err := scmTools.ImageTooBig(image)
-// 			if err == nil && !toobig {
-// 				return image, nil
-// 			}
-// 		}
-// 	}
-// 	return "", errors.New("Failed to find a goose image")
-// }
-
-// func formatResponse(image string) (string, error) {
-// 	img, err := url.Parse(image)
-// 	if err != nil {
-// 		return "", fmt.Errorf("invalid image url %s: %v", image, err)
-// 	}
-// 	return fmt.Sprintf("![goose image](%s)", img), nil
-// }
+func formatResponse(image string) (string, error) {
+	img, err := url.Parse(image)
+	if err != nil {
+		return "", fmt.Errorf("invalid image url %s: %v", image, err)
+	}
+	return fmt.Sprintf("![goose image](%s)", img), nil
+}
--- pkg/chatbot/plugins/wip/wip-label.go
+++ pkg/chatbot/plugins/wip/wip-label.go
@@ -1,95 +1,72 @@
-/*
-Copyright 2017 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Package wip will label a PR a work-in-progress if the author provides
-// a prefix to their pull request title to the same effect. The submit-
-// queue will not merge pull requests with the work-in-progress label.
-// The label will be removed when the title changes to no longer begin
-// with the prefix.
 package wip
 
-// const (
-// 	// PluginName defines this plugin's registered name.
-// 	pluginName = "wip"
-// 	label      = "do-not-merge/work-in-progress"
-// )
+import (
+	"fmt"
+	"regexp"
 
-// var (
-// 	titleRegex = regexp.MustCompile(`(?i)^\W?WIP\W`)
-// )
+	"github.com/eddycharly/kloops/pkg/chatbot/plugins"
+	"github.com/jenkins-x/go-scm/scm"
+)
 
-// func init() {
-// 	plugins.RegisterHelpProvider(pluginName, helpProvider)
-// 	plugins.RegisterPullRequestHandler(pluginName, handlePullRequest)
-// }
+const (
+	// PluginName defines this plugin's registered name.
+	pluginName = "wip"
+	label      = "do-not-merge/work-in-progress"
+)
 
-// func helpProvider(config *v1alpha1.PluginConfigSpec) (*pluginhelp.PluginHelp, error) {
-// 	// Only the Description field is specified because this plugin is not triggered with commands and is not configurable.
-// 	return &pluginhelp.PluginHelp{
-// 			Description: "The wip (Work In Progress) plugin applies the '" + label + "' Label to pull requests whose title starts with 'WIP' or are in the 'draft' stage, and removes it from pull requests when they remove the title prefix or become ready for review. The '" + label + "' Label is typically used to block a pull request from merging while it is still in progress.",
-// 		},
-// 		nil
-// }
+var (
+	titleRegex = regexp.MustCompile(`(?i)^\W?WIP\W`)
+)
 
-// // Strict subset of SCM.Client methods.
-// type scmClient interface {
-// 	AddLabel(string, int, string) error
-// 	RemoveLabel(string, int, string) error
-// 	GetLabels(string, int) ([]*scm.Label, error)
-// }
+func init() {
+	plugins.RegisterPlugin(
+		pluginName,
+		plugins.Plugin{
+			Description:        "The wip (Work In Progress) plugin applies the '" + label + "' Label to pull requests whose title starts with 'WIP' or are in the 'draft' stage, and removes it from pull requests when they remove the title prefix or become ready for review. The '" + label + "' Label is typically used to block a pull request from merging while it is still in progress.",
+			PullRequestHandler: handlePullRequest,
+		},
+	)
+}
 
-// func handlePullRequest(request plugins.PluginRequest, event *scm.PullRequestHook) error {
-// 	logger := request.Logger()
-// 	scmClient := request.ScmClient()
-// 	return handle(scmClient.PullRequests, logger, event.Repository(), event.Action, event.PullRequest)
-// }
+func handlePullRequest(request plugins.PluginRequest, pe scm.PullRequestHook) error {
+	// These are the only actions indicating the PR title may have changed.
+	if pe.Action != scm.ActionOpen &&
+		pe.Action != scm.ActionReopen &&
+		pe.Action != scm.ActionEdited &&
+		pe.Action != scm.ActionUpdate &&
+		pe.Action != scm.ActionReadyForReview {
+		return nil
+	}
+	return handle(request, pe)
+}
 
-// func handle(client scmClient, logger logr.Logger, repo scm.Repository, action scm.Action, pr scm.PullRequest) error {
-// 	// These are the only actions indicating the PR title may have changed.
-// 	if action != scm.ActionOpen &&
-// 		action != scm.ActionReopen &&
-// 		action != scm.ActionEdited &&
-// 		action != scm.ActionUpdate &&
-// 		action != scm.ActionReadyForReview {
-// 		return nil
-// 	}
+func handle(request plugins.PluginRequest, pe scm.PullRequestHook) error {
+	scmClient := request.ScmClient()
+	logger := request.Logger()
 
-// 	currentLabels, err := client.GetLabels(repo.FullName, pr.Number)
-// 	if err != nil {
-// 		return fmt.Errorf("could not get labels for PR %s:%d in WIP plugin: %v", repo.FullName, pr.Number, err)
-// 	}
-// 	hasLabel := false
-// 	for _, l := range currentLabels {
-// 		if l.Name == label {
-// 			hasLabel = true
-// 		}
-// 	}
+	currentLabels, err := scmClient.PullRequests.GetLabels(pe.PullRequest.Base.Repo.FullName, pe.PullRequest.Number)
+	if err != nil {
+		return fmt.Errorf("could not get labels for PR %s:%d in WIP plugin: %v", pe.PullRequest.Base.Repo.FullName, pe.PullRequest.Number, err)
+	}
+	hasLabel := false
+	for _, l := range currentLabels {
+		if l.Name == label {
+			hasLabel = true
+		}
+	}
 
-// 	needsLabel := pr.Draft || titleRegex.MatchString(pr.Title)
+	needsLabel := pe.PullRequest.Draft || titleRegex.MatchString(pe.PullRequest.Title)
 
-// 	if needsLabel && !hasLabel {
-// 		if err := client.AddLabel(repo.FullName, pr.Number, label); err != nil {
-// 			logger.Error(err, "error while adding label")
-// 			return err
-// 		}
-// 	} else if !needsLabel && hasLabel {
-// 		if err := client.RemoveLabel(repo.FullName, pr.Number, label); err != nil {
-// 			logger.Error(err, "error while removing label")
-// 			return err
-// 		}
-// 	}
-// 	return nil
-// }
+	if needsLabel && !hasLabel {
+		if err := scmClient.PullRequests.AddLabel(pe.PullRequest.Base.Repo.FullName, pe.PullRequest.Number, label); err != nil {
+			logger.Error(err, "error while adding Label")
+			return err
+		}
+	} else if !needsLabel && hasLabel {
+		if err := scmClient.PullRequests.RemoveLabel(pe.PullRequest.Base.Repo.FullName, pe.PullRequest.Number, label); err != nil {
+			logger.Error(err, "error while removing Label")
+			return err
+		}
+	}
+	return nil
+}
